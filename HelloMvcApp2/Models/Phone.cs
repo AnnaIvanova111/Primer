5f94f338d25e924d07be319bef433683d6083150
AnnaIvanova111 <43719830+AnnaIvanova111@users.noreply.github.com>
2020-02-17 09:42:26 +0300

    Add files via upload

---
+++ HelloMvcApp2/Models/Phone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloMvcApp2.Models
+{
+    public class Phone
+    {
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public int Price { get; set; }
+    }
+}
