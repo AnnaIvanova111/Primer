c01b827a401ffa9c28b3cbd5ef027fa133679f99
AnnaIvanova111 <43719830+AnnaIvanova111@users.noreply.github.com>
2020-03-02 15:44:32 +0300

    Add files via upload

---
+++ HtmlHelpersApp.App_Code/HtmlHelpersApp.App_Code/App_Code/ListHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Encodings.Web;
+
+namespace HtmlHelpersApp.App_Code
+{
+    public static class ListHelper
+    {
+        public static HtmlString CreateList(this IHtmlHelper html, string[] items)
+        {
+            TagBuilder ul = new TagBuilder("ul");
+            foreach (string item in items)
+            {
+                TagBuilder li = new TagBuilder("li");
+                // добавляем текст в li
+                li.InnerHtml.Append(item);
+                // добавляем li в ul
+                ul.InnerHtml.AppendHtml(li);
+            }
+            ul.Attributes.Add("class", "itemsList");
+            var writer = new System.IO.StringWriter();
+            ul.WriteTo(writer, HtmlEncoder.Default);
+            return new HtmlString(writer.ToString());
+        }
+    }
+}
